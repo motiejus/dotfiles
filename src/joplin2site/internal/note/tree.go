66051aa742bf2700182562db2979b4ee765004e0
Motiejus Jakštys <desired.mta@gmail.com>
2020-06-07 23:50:14 +0300

    wip trees

--- src/joplin2site/internal/note/tree.go
+++ src/joplin2site/internal/note/tree.go
@@ -3,10 +3,9 @@ package note
 import (
 	"fmt"
 	"io/ioutil"
-	"time"
 )
 
-type Notes map[string]Note
+type Notes map[string]*Note
 
 func ListNotes(dir string) (Notes, error) {
 	files, err := ioutil.ReadDir(dir)
@@ -24,74 +23,34 @@ func ListNotes(dir string) (Notes, error) {
 			return nil, fmt.Errorf("failed to parse %q: %w", file.Name(), err)
 		}
 
-		notes[note.ID] = note
+		notes[note.ID] = &note
 	}
 
 	return notes, nil
 }
 
-// SubPages returns immediate Pages of a tree
-func (n Notes) SubPages(name string) (Notes, error) {
-	// Find the parent note that will be the parent of the sub-notebook.
-	var parentID string
-	for _, note := range n {
-		if note.Type != ItemTypeFolder {
-			continue
-		}
-		if note.Title == name {
-			parentID = note.ID
-		}
-	}
-
-	if parentID == "" {
-		return nil, fmt.Errorf("sub-page %q not found", name)
-	}
-
-	var retPages []Page
-	for _, inote := range t.Notes {
-		if inote.ParentID != parentID {
-			continue
-		}
-		if inote.Type != note.ItemTypeNote {
-			continue
-		}
-
-		page, err := FromNote(inote)
-		if err != nil {
-			return nil, fmt.Errorf("failed to convert note to page: %w", err)
-		}
-		if page.PublishedAt.After(time.Now()) {
-			continue
-		}
-
-		retPages = append(retPages, page)
-	}
-	return retPages, nil
-}
-
 // Shake returns a sub-set of notes which eventually parent to "tld"
-func (n Notes) Shake(tld string) (Notes, error) {
-	tree := buildTree(n)
-	topID := n.getFolderID(tld)
+func (notes Notes) Shake(tld string) (Notes, error) {
+	topID := notes.GetFolderID(tld)
 	if topID == "" {
 		return nil, fmt.Errorf("tld %q not found", tld)
 	}
 
 	children := make(children)
-	tree.flatten(topID, children)
+	buildTree(notes).flatten(topID, children)
 
-	ret := make(map[string]note.Note, len(children))
+	ret := make(Notes, len(children))
 	for noteID := range children {
-		ret[noteID] = n[noteID]
+		ret[noteID] = notes[noteID]
 	}
 
 	return ret, nil
 }
 
-// getFolderID returns a folder ID for a particular title
-func (n Notes) getFolderID(title string) string {
+// GetFolderID returns a folder ID for a particular title
+func (notes Notes) GetFolderID(title string) string {
 	var parentID string
-	for _, note := range n {
+	for _, note := range notes {
 		if note.Type != ItemTypeFolder {
 			continue
 		}
@@ -118,9 +77,9 @@ func buildTree(notes Notes) tree {
 }
 
 // flatten returns all children of a tree
-func (t tree) flatten(id string, acc tree) {
+func (t tree) flatten(id string, acc children) {
 	acc[id] = struct{}{}
 	for child := range t {
-		flatten(child, acc)
+		t.flatten(child, acc)
 	}
 }
