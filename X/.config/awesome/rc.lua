057358565a3b3ad1d16d147e8cb2483997edb72c
Motiejus Jakštys <motiejus@uber.com>
2021-04-01 15:08:25 +0300

    random dotfile updates

--- X/.config/awesome/rc.lua
+++ X/.config/awesome/rc.lua
@@ -63,7 +63,7 @@ modkey = "Mod4"
 awful.layout.layouts = {
     --awful.layout.suit.floating,
     awful.layout.suit.tile,
-    --awful.layout.suit.tile.left,
+    awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
     --awful.layout.suit.tile.top,
     --awful.layout.suit.fair,
